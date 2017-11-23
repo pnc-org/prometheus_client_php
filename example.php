66eb841d9f67c4f1122ee35ae93c6b65671f6f1b
Kristijan Burnik <kristijanburnik@gmail.com>
2017-11-23 13:45:47 +0100

    Add more to example

--- example.php
+++ example.php
@@ -20,6 +20,16 @@ $registry = new CollectorRegistry(new InMemory(/*storeToFile=*/true));
 $counter = $registry->registerCounter('test', 'some_counter', 'it increases', array('type'));
 $counter->incBy(3, array('blue'));
 
+
+$gauge = $registry->getOrRegisterGauge('test', 'some_gauge', 'it sets', array('type'));
+$gauge->set(2.5, ['blue']);
+
+$histogram = $registry->getOrRegisterHistogram(
+    'test', 'some_histogram', 'it observes',
+    array('type'),
+    array(0.1, 1, 2, 3.5, 4, 5, 6, 7, 8, 9));
+$histogram->observe(3.5, array('blue'));
+
 $renderer = new RenderTextFormat();
 $result = $renderer->render($registry->getMetricFamilySamples());
 
